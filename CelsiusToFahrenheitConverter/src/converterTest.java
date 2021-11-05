e9e1857c822e4e8c0360aef79b406448168722b0
Iaquine <61883817+Iaquine@users.noreply.github.com>
2021-11-05 11:53:24 -0300

    Update mockito dependency

--- CelsiusToFahrenheitConverter/src/converterTest.java
+++ CelsiusToFahrenheitConverter/src/converterTest.java
@@ -3,6 +3,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import static org.mockito.Mockito.*;
 
 class converterTest {
 

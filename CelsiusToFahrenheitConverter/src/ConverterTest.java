b43b47c7d7b9fed1adbcabc9865b2065ac137781
Iaquine <61883817+Iaquine@users.noreply.github.com>
2021-11-06 20:10:05 -0300

    Code refactored to show all test cases

--- CelsiusToFahrenheitConverter/src/ConverterTest.java
+++ CelsiusToFahrenheitConverter/src/ConverterTest.java
@@ -3,15 +3,60 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ConverterTest {
 
+	
+	
 	Converter con = new Converter();
 
+	@Nested
+    @DisplayName("Tests if the right temperature is been returned")
+    class getConvertedTemperature {
+		
+		@Test
+		@DisplayName("to 32 returns 0")
+		void to32returns0() {
+			double convertedTemp = con.fahrenheitToCelsius(32.0);
+			assertEquals(0,convertedTemp);
+		}
+		@Test
+		@DisplayName("to 50 returns 10")
+		void to50returns10() {
+			double convertedTemp = con.fahrenheitToCelsius(50.0);
+			assertEquals(10.0,convertedTemp);
+		}
+		
+		@Test
+		@DisplayName("to 212 returns 100")
+		void to212returns100() {
+			double convertedTemp = con.fahrenheitToCelsius(212.0);
+			assertEquals(100.0,convertedTemp);
+		}
+		
+		@Test
+		@DisplayName("to -40 returns -40")
+		void toMinus40returnsMinus40() {
+			double convertedTemp = con.fahrenheitToCelsius(-40.0);
+			assertEquals(-40.0,convertedTemp);
+		}
+		
+		@Test
+		@DisplayName("to -459.67 returns -273.15")
+		void toMinus459returnsMinus273() {
+			double convertedTemp = con.fahrenheitToCelsius(-459.67);
+			assertEquals(-273.15,convertedTemp);
+		}
+		
+	}
+	
+	
+	//here the code is cleaner but doesn't all the tests in the suite
 	@Test
 	@DisplayName("Tests if the right temperature is been returned") 
-	public void getConvertedTemperature() {
+	void getConvertedTemperature() {
 		double convertedTemp = 0;
 		
 		convertedTemp = con.fahrenheitToCelsius(32.0);

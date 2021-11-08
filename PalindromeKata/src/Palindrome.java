ab72088cc7341265ee5c231b9293f3de8e88599b
Iaquine <61883817+Iaquine@users.noreply.github.com>
2021-11-08 12:19:48 -0300

    Implemented isPalindrome

--- PalindromeKata/src/Palindrome.java
+++ PalindromeKata/src/Palindrome.java
@@ -10,6 +10,21 @@ public class Palindrome {
 		
 		boolean Palindrome = true;  
 		
+
+		String firstchar = string;
+        int length = firstchar.length();
+         
+        for(int beginIndex = 0; beginIndex < length; beginIndex++)
+        {
+            if(firstchar.charAt(beginIndex) != firstchar.charAt(length-1-beginIndex)) {
+                System.out.println("String is not a palindrome.");
+                Palindrome = false;
+                break;
+            }
+        }
+		
+		
+		
 		return Palindrome;
 	}
 

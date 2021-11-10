371121b2cc68ccdb76b1a94366f52d5e6c184caf
Iaquine <61883817+Iaquine@users.noreply.github.com>
2021-11-10 17:19:56 -0300

    prime factors for 2

--- PrimeFactorsKata/src/PrimeFactors.java
+++ PrimeFactorsKata/src/PrimeFactors.java
@@ -1,13 +1,11 @@
-
-
 public class PrimeFactors {
 
-	int [] factors =  new int[0];
+	int [] factors =  new int[1];
 
 
-	public Object findPrimeFactors(int i) {
-		//if (i == 1) return null;
-		return factors;
+	public Object findPrimeFactors(Object number) {
+		if (number.equals(1)) return new int[0];
+		return factors[0]=(int) number;
 	}
 
 
--- PrimeFactorsKata/src/PrimeFactorsTest.java
+++ PrimeFactorsKata/src/PrimeFactorsTest.java
@@ -10,12 +10,12 @@ class PrimeFactorsTest {
 	
 	@DataProvider (name = "data-provider")
     public Object[][] dpMethod(){
-	 return new Object[][] {{new int [0]}};
+	 return new Object[][] {{new int [0], 1}, {2, 2}};
     }
 	
 	@Test (dataProvider = "data-provider")
-	public void returnNoneFor1 (Object val) {
-	  assertEquals(val, prime.findPrimeFactors(1));
+	public void returnNoneFor1 (Object val, Object number) {
+	  assertEquals(val, prime.findPrimeFactors(number));
 	}
 
 }

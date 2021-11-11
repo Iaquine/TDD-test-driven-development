3a027ebef7de9791a226a53cc060264324e335af
Iaquine <61883817+Iaquine@users.noreply.github.com>
2021-11-11 14:27:14 -0300

    error test 3

--- PrimeFactorsKata/src/PrimeFactors.java
+++ PrimeFactorsKata/src/PrimeFactors.java
@@ -1,11 +1,28 @@
 public class PrimeFactors {
-
-	int [] factors =  new int[1];
+	
+	int count = 0;
+	int [] factors = new int[10];
 
 
 	public Object findPrimeFactors(Object number) {
+		
 		if (number.equals(1)) return new int[0];
-		return factors[0]=(int) number;
+		
+		if ((int)number > 1) {
+			if ((int)number % 2 == 0) {
+				count = count + 1;
+				factors[count] = 2;
+				//count = count + 1;
+				number = (int)number/2;
+			}
+			if ((int)number > 1) {
+				count = count + 1;
+				factors[count] = (int)number;
+				//count = count + 1;
+			}
+		}
+		
+		return factors;
 	}
 
 
--- PrimeFactorsKata/src/PrimeFactorsTest.java
+++ PrimeFactorsKata/src/PrimeFactorsTest.java
@@ -5,16 +5,17 @@ import org.testng.annotations.Test;
 class PrimeFactorsTest {
 
 	PrimeFactors prime = new PrimeFactors();
-	int [] primeFactores = new int [0];
-	
+	int [] primeFactores = {2,2};
+	int [] primeFactor = {};
 	
 	@DataProvider (name = "data-provider")
     public Object[][] dpMethod(){
-	 return new Object[][] {{new int [0], 1}, {2, 2}, {3,3}};
+	 return new Object[][] {
+		 {new int [0], 1}, {2, 2}, {3,3}, {primeFactores, 4}};
     }
 	
 	@Test (dataProvider = "data-provider")
-	public void returnNoneFor1 (Object val, Object number) {
+	public void testcases (Object val, Object number) {
 	  assertEquals(val, prime.findPrimeFactors(number));
 	}
 
